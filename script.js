2e8ee5a0b12e465e5d362474022e059f5ab52897
Evgeny <evgeny.gook@gmail.com>
2019-02-02 17:57:24 +0300

    Homework

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
  let question = prompt( "Ваш бюджет на месяц?"),
-     date = prompt("YYYY-MM-DD");
+     date = prompt("YYYY-MM-DD"),
+     count = 2;
 
      
 var appData = {
     buget: question,
     timeData: date,
+    savings: false,
+
+    expenses:{},
+    optionalExpenses:{},
+    income:[],
 };     
 
-var expenses = {};
-var optionalExpenses ={};
+function Question(count){
+    for(i=0; i< count; i++ ){
+        var whatExpenses = prompt("Введите обязательную статью расходов в этом месяце");
+        var howMuch = prompt("Введите обязательную статью расходов в этом месяце");
+        return appData.expenses[i].whatExpenses=howMuch;
+    }
+}
+
+console.log(appData.expenses);
