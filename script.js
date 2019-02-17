b18aa62532f5c403cef2fa3164f9beea2f0293f0
Evgeny <evgeny.gook@gmail.com>
2019-02-17 20:13:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -24,9 +24,43 @@ let appData = {
                 appData.expenses[whatExpenses]=howMuch;
         }else{
                 alert("Error");
-                }
-        }
+                i--;
+        }       
+    }  
 
+
+        //while
+        /*var i = 0;
+        while (i < 2){
+                console.log("работает цикл")
+                let whatExpenses = prompt("Введите обязательную статью расходов в этом месяце",'');
+                let howMuch = prompt("Во сколько обойдется?",'');
+         
+                if(whatExpenses !== null && howMuch !== null && whatExpenses !== '' && howMuch !== ''){
+                        appData.expenses[whatExpenses]=howMuch;
+                        i++;
+                }else{
+                        alert("Error");
+                        i--;
+                        }
+                
+        } 
+
+        //do_while
+        i=0;
+        do {
+        console.log("работает цикл")
+                let whatExpenses = prompt("Введите обязательную статью расходов в этом месяце",'');
+                let howMuch = prompt("Во сколько обойдется?",'');
+         
+                if(whatExpenses !== null && howMuch !== null && whatExpenses !== '' && howMuch !== ''){
+                        appData.expenses[whatExpenses]=howMuch;
+                        i++;
+                }else{
+                        alert("Error");
+                        i--;
+                        }
+                } while(i< 2); */
 
 appData.moneyPerDay = appData.bujet/ 30;
 
@@ -37,3 +71,6 @@ appData.moneyPerDay = appData.bujet/ 30;
         }else{appData.moneyPerDay>5000
                 alert("Больше");
         }
+
+
+        /* Бывает 3 цикла */
